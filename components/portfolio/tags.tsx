13e726868c99c5bd346175a08c82de82b8f370c6
wells <jhang0912407249@gmail.com>
2022-03-01 00:53:16 +0800

    Fix portfolio tag wrong display in dark mode

--- components/portfolio/tags.tsx
+++ components/portfolio/tags.tsx
@@ -6,7 +6,7 @@ export default function Tags({tags, queryTag}:{tags:string[], queryTag:string|st
     <ul className="flex justify-center mt-20">
     { tags.map((tag, index) =>
     <li key={ tag } className={`px-6 text-base text-center  ${(index == tags.length -1)? "": "border-r-2 border-primary dark:border-white"}`}>
-        <button className={`${queryTag == tag? "": "text-opacity-40"} text-primary dark:text-white`} onClick={() => addQuery(tag)} >{ tag }</button>
+      <button className={`${queryTag == tag? "": "dark:text-opacity-40 text-opacity-40"} text-primary dark:text-white`} onClick={() => addQuery(tag)} >{ tag }</button>
     </li>
     )}
   </ul>
