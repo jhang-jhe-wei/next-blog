5dccc25f692dee715762c1c27aeea23e0868be4a
wells <jhang0912407249@gmail.com>
2022-03-25 13:34:47 +0800

    Add superStruct in about

--- lib/about.ts
+++ lib/about.ts
@@ -1,9 +1,42 @@
 import fs from 'fs'
 import path from 'path'
 import YAML from 'yaml'
+import { assert, object, number, boolean, string, array } from 'superstruct'
 
 const filePath = path.join(process.cwd(), 'data', 'about.yml')
+const AboutModel = object({
+  education: array(object({
+    title: string(),
+    subTitle: string(),
+    startedAt: string(),
+    endedAt: string(),
+    highlights: array(string())
+  })),
+  works: array(object({
+    title: string(),
+    subTitle: string(),
+    startedAt: string(),
+    endedAt: string(),
+    highlights: array(string())
+  })),
+  achievements: array(object({
+    title: string(),
+    isFull: boolean(),
+    highlights: array(string())
+  })),
+  skillsList: array(object({
+    title: string(),
+    skills: array(object({
+      name: string(),
+      proficiency: number()
+    }))
+    }
+  ))
+})
+
 export async function getAboutData() {
   const file = fs.readFileSync(filePath, 'utf8')
-  return YAML.parse(file)
+  const data = YAML.parse(file)
+  assert(data, AboutModel)
+  return data
 }
