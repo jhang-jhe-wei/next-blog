7b948eb1b6ff4cbaa6d2f049fc37c36cdd6bc99e
wells <jhang0912407249@gmail.com>
2022-03-25 12:39:56 +0800

    Add scrollTo anchor in toc

--- components/about/toc.tsx
+++ components/about/toc.tsx
@@ -5,7 +5,11 @@ export default function Toc():React.ReactElement {
     <ul className="flex justify-center mt-20">
     { tags.map((tag, index) =>
     <li key={ tag } className={`px-6 text-base text-center  ${(index == tags.length -1)? "": "border-r-2 border-primary dark:border-white"}`}>
-      <a href={`#${anchors[index]}`} className="dark:text-opacity-40 dark:hover:text-opacity-100 text-opacity-40 hover:text-opacity-100 text-primary dark:text-light">{ tag }</a>
+      <button onClick={()=>{
+        window.scrollTo({ top: document.getElementById(anchors[index]).offsetTop - window.innerHeight / 2, behavior: 'smooth' }) } }
+        className="dark:text-opacity-40 dark:hover:text-opacity-100 text-opacity-40 hover:text-opacity-100 text-primary dark:text-light">
+        { tag }
+      </button>
     </li>
     )}
   </ul>
