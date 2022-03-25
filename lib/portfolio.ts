5c91b85c7abc6dc75fdc95675b66c36b8db126d8
wells <jhang0912407249@gmail.com>
2022-03-25 13:35:06 +0800

    Add superStruct in portfolio

--- lib/portfolio.ts
+++ lib/portfolio.ts
@@ -1,9 +1,21 @@
 import fs from 'fs'
 import path from 'path'
 import YAML from 'yaml'
+import { assert, object, string, array } from 'superstruct'
 
 const filePath = path.join(process.cwd(), 'data', 'portfolio.yml')
+const PortfolioModel = array(object({
+  title: string(),
+  tag: string(),
+  image: string(),
+  description: string(),
+  demo_link: string(),
+  code_link: string()
+}))
+
 export async function getPortfolioData() {
   const file = fs.readFileSync(filePath, 'utf8')
-  return YAML.parse(file)
+  const data = YAML.parse(file)
+  assert(data, PortfolioModel)
+  return data
 }
