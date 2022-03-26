52d4a4c69264ee0a936d257903d65928cd79b5d2
wells <jhang0912407249@gmail.com>
2022-03-26 17:56:59 +0800

    Add RWD in nav

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -2,21 +2,27 @@ import Link from 'next/link'
 import NavItem from './nav_item'
 import NavBtnItem from './nav_btn_item'
 import NavIconItem from './nav_icon_item'
+import NavHamburger from './nav_hamburger_item'
+import NavMobileMenu from './nav_mobile_menu'
+import { useState } from 'react'
 
 export default function Nav():React.ReactElement{
+  const [reveal, setReveal] = useState(false);
   return (
-    <nav className="sticky top-0 z-50 px-10 py-6 bg-light dark:bg-primary bg-opacity-60 dark:bg-opacity-60 backdrop-blur dark:backdrop-blur">
+    <nav className="sticky top-0 z-30 px-10 py-6 bg-light dark:bg-primary bg-opacity-60 dark:bg-opacity-60 backdrop-blur dark:backdrop-blur">
       <div className="flex items-center justify-between mx-auto">
         <Link href="/" ><a className="text-2xl text-primary dark:text-white hover:no-underline">Wells</a>
         </Link>
-        <ul className="flex items-center justify-between">
+        <ul className="items-center justify-between hidden sm:flex">
           <NavItem href="/portfolio">作品集</NavItem>
           <NavItem href="/about">經歷</NavItem>
           <NavItem href="https://blog.wells.tw">部落格</NavItem>
-          <NavBtnItem href="#">聯絡我</NavBtnItem>
+          <NavBtnItem href="mailto:jhang0912407249@gmail.com">聯絡我</NavBtnItem>
           <NavIconItem/>
         </ul>
+        <NavHamburger reveal={reveal} setReveal={setReveal}/>
       </div>
+      <NavMobileMenu reveal={reveal}/>
     </nav>
   )
 }
---
+++ components/nav/nav_hamburger_item.tsx
@@ -0,0 +1,9 @@
+export default function NavHamburger({reveal, setReveal}:{reveal:boolean, setReveal:Function}) {
+  return (
+    <div className="absolute z-50 flex flex-col justify-between h-6 right-10 sm:hidden" onClick={()=>setReveal((reveal:boolean) => reveal = !reveal)}>
+      <span className={`block w-8 h-1 transition-all duration-500 ${reveal? "-rotate-45 translate-y-2.5 bg-light":"bg-primary dark:bg-light"}`}></span>
+      <span className={`block w-8 h-1 bg-primary dark:bg-light transition-all duration-500 ${reveal? "hidden":""}`}></span>
+      <span className={`block w-8 h-1 transition-all duration-500 ${reveal? "rotate-45 -translate-y-2.5 bg-light":"bg-primary dark:bg-light"}`}></span>
+    </div>
+  )
+}
