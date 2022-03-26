881d4ae5f183e050d10a8e8e1f0acff02235a1ba
wells <jhang0912407249@gmail.com>
2022-03-26 14:44:20 +0800

    Change resume btn layout

--- components/about/resume.tsx
+++ components/about/resume.tsx
@@ -1,8 +1,8 @@
 export default function Resume() {
   return (
     <div className="pb-32 mt-32 text-center">
-      <h2 className="mb-10 text-center text-white font-notosans shadow-gray-700 text-7xl">Download</h2>
-      <a target="_blank" href="https://www.cakeresume.com/jhang0912407249" className="border-2 border-cyan-450 text-white px-5 pt-3 pb-2 rounded-[10px] bg-cyan-450 dark:hover:border-primary hover:bg-white hover:text-cyan-450 hover:no-underline">
+      <h2 className="text-center text-white font-notosans shadow-gray-700 text-7xl">Download</h2>
+      <a target="_blank" href="https://www.cakeresume.com/jhang0912407249" className="mt-5 inline-block border-2 border-cyan-450 text-white px-5 pt-3 pb-2 rounded-[10px] bg-cyan-450 dark:hover:border-primary hover:bg-white hover:text-cyan-450 hover:no-underline">
         觀看我的履歷
       </a>
     </div>
