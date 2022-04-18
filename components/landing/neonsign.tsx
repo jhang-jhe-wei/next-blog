10fae4c127add7c2ce024e5777526d0ba309984e
wells <jhang0912407249@gmail.com>
2022-04-19 00:28:46 +0800

    Fix requestAnimationFrame cannot cancel

--- components/landing/neonsign.tsx
+++ components/landing/neonsign.tsx
@@ -11,7 +11,7 @@ export default function Neonsign({width}:{width:string}){
   useEffect(()=>{
     loop(0);
     return () => {cancelAnimationFrame(animationId.current)}
-  }, [element])
+  })
 
   return (
     <svg className="mx-auto" width={width} viewBox="0 0 579 143" fill="none" xmlns="http://www.w3.org/2000/svg">
