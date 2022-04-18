e3c43ec7a98516a696649ed9d1b28b784a3f4b15
wells <jhang0912407249@gmail.com>
2022-04-18 22:23:57 +0800

    Update project card layout in project

--- components/about/project.tsx
+++ components/about/project.tsx
@@ -9,11 +9,11 @@ export default function Project({project, index}: ProjectComponentProps) {
 
   return (
     <li ref={element} className={`bg-white dark:bg-primary-opacity mx-auto max-w-xs xl:max-w-full flex xl:flex-row flex-col mb-10 transition-opacity-and-transform duration-500 ${reveal? 'opacity-100' : 'translate-y-20 opacity-0'} ${index % 2 == 0? "": "xl:flex-row-reverse"}`}>
-      <img className="max-w-xs bg-white max-h-72" src={project.image} alt=""/>
+      <img className="h-80 w-80 object-center" src={project.image} alt=""/>
       <div className="flex flex-col justify-between px-7 py-7">
         <div>
-          <h3 className="text-xl text-secondary">{project.title}</h3>
-          <p className="mt-5 text-primary dark:text-white">{project.description}</p>
+          <h3 className="font-bold text-xl text-secondary">{project.title}</h3>
+          <p className="line-clamp-8 mt-5 text-primary dark:text-white">{project.description}</p>
         </div>
         <div className="mt-5">
           {
