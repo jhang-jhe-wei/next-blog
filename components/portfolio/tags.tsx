e60f39160282ceca391a0f5f6e6630a3e9ebbcba
wells <jhang0912407249@gmail.com>
2022-04-19 11:57:16 +0800

    Update layout in portfolio layout

--- components/portfolio/tags.tsx
+++ components/portfolio/tags.tsx
@@ -16,10 +16,10 @@ export default function Tags({tags, queryTag}:TagsData): React.ReactElement{
   }, [])
 
   return(
-    <ul className="flex justify-center mt-20">
+    <ul className="flex mx-auto mt-20 overflow-x-auto w-fit">
     { tags.map((tag, index) =>
-      <li key={ tag } className={`px-6 text-base text-center  ${(index == tags.length -1)? "": "border-r-2 border-primary dark:border-white"}`}>
-        <button className={`${queryTag == tag? "": "dark:text-opacity-40 text-opacity-40"} text-primary dark:text-white`} onClick={() => addQuery(tag)} >{ tag }</button>
+      <li key={ tag } className={`px-6 text-base text-center ${(index == tags.length -1)? "": "border-r-2 border-primary dark:border-white"}`}>
+        <button className={`${queryTag == tag? "": "dark:text-opacity-40 text-opacity-40"} text-primary dark:text-white whitespace-nowrap`} onClick={() => addQuery(tag)} >{ tag }</button>
       </li>)
     }
   </ul>
