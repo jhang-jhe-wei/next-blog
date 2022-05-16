45749f0756992c398c83d4c2d89cd70435f4b719
wells <jhang0912407249@gmail.com>
2022-05-16 20:55:30 +0800

    Add click events in ButtonBox

--- components/landing/button_box.tsx
+++ components/landing/button_box.tsx
@@ -1,10 +1,15 @@
-export default function ButtonBox({ x, y }:{ x:string, y:string }){
+interface ButtonBox{
+  x: string;
+  y: string;
+  clickHandlers?: any
+}
+export default function ButtonBox({ x, y, clickHandlers}:ButtonBox){
   return (
     <svg x={x} y={y} width="40" height="70" viewBox="0 0 40 70" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect width="40" height="70" className="stroke-primary dark:stroke-white" strokeWidth="4"/>
-      <circle cx="20" cy="17.5" r="6" fill="#FF4452"/>
-      <circle cx="20" cy="35" r="6" fill="#F8FF17"/>
-      <circle cx="20" cy="52.5" r="6" fill="#00AC62"/>
+      <circle cx="20" cy="17.5" r="6" fill="#FF4452" onClick={clickHandlers[0]}/>
+      <circle cx="20" cy="35" r="6" fill="#F8FF17" onClick={clickHandlers[1]}/>
+      <circle cx="20" cy="52.5" r="6" fill="#00AC62" onClick={clickHandlers[2]}/>
     </svg>
   )
 }
--- components/landing/pipeline_diagram.tsx
+++ components/landing/pipeline_diagram.tsx
@@ -4,7 +4,11 @@ import DistributionBoard from "./distribution_board"
 import Beeper from "./beeper"
 import SwitchBox from "./switch_box"
 import SocialMediaList from "../social_media_list/social_media_list"
+import { useAppDispatch } from "../../reducers/store"
+import { changeColor, changeStrokeColor } from "../../reducers/neonsign/neonsign_slice"
+
 export default function PipelineDiagram(){
+  const dispatch = useAppDispatch();
   return (
     <svg viewBox="0 0 800 500" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M0 90 H370" strokeWidth="10" className="stroke-primary dark:stroke-white"/>
@@ -20,13 +24,27 @@ export default function PipelineDiagram(){
       <SwitchBox x="730" y="320"/>
 
       <path d="M0 335 H180" strokeWidth="10" className="stroke-primary dark:stroke-white"/>
-      <ButtonBox x="180" y="300"/>
+      <ButtonBox x="180" y="300" clickHandlers={[
+        () => dispatch(changeColor("red")),
+        () => dispatch(changeColor("yellow")),
+        () => dispatch(changeColor("green"))
+      ]}/>
 
       <path d="M0 210 H240 Q270 210, 270 240 V300 " strokeWidth="3" className="stroke-primary dark:stroke-white"/>
-      <ButtonBox x="250" y="300"/>
+      <ButtonBox x="250" y="300" clickHandlers={[
+        () => dispatch(changeStrokeColor("red")),
+        () => dispatch(changeStrokeColor("yellow")),
+        () => dispatch(changeStrokeColor("green"))
+      ]}/>
+
 
       <path d="M0 140 H260 Q270 140, 280 150 L330 200 Q340 210, 340 220 V300" strokeWidth="3" className="stroke-primary dark:stroke-white"/>
-      <ButtonBox x="320" y="300"/>
+      <ButtonBox x="320" y="300" clickHandlers={[
+        () => dispatch(changeColor("red")),
+        () => dispatch(changeColor("yellow")),
+        () => dispatch(changeColor("green"))
+      ]}/>
+
 
       <path d="M0 480 H380" strokeWidth="5" className="hidden lg:block stroke-primary dark:stroke-white"/>
       <foreignObject className="hidden lg:block" x="370" y="440" width="430" height="60">
