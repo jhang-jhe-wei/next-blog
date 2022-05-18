46ab2d478b2163fa4f37f501e91536abd202a8be
PastLeo <chgu82837@gmail.com>
2022-05-18 22:16:32 +0800

    demo using reduxThunk

--- components/nav/nav_icon_item.tsx
+++ components/nav/nav_icon_item.tsx
@@ -1,16 +1,10 @@
 import { useAppDispatch, useAppSelector } from "../../reducers/store";
-import { trunAllOn, trunAllOff } from "../../reducers/light/light_slice";
-import { switchMode } from "../../reducers/mode/mode_slice";
+import { navIconItemClicked } from "../../reducers/actions";
 export default function NavIconItem():React.ReactElement{
   const dispatch = useAppDispatch();
   const mode = useAppSelector(state => state.mode.value)
   const clickHandler = ()=>{
-    if(mode === "light"){
-      dispatch(trunAllOn());
-    }else{
-      dispatch(trunAllOff());
-    }
-    dispatch(switchMode());
+    dispatch(navIconItemClicked());
   }
 
   return (
