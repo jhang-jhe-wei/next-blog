0389ec887758a50c9e61fd50d718e984a675e498
wells <jhang0912407249@gmail.com>
2022-05-19 22:14:09 +0800

    Change image url in html head

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta property="og:title" content="Wells's website" />
-        <meta property="og:image" content="https://imgur.com/qtzOumT" />
+        <meta property="og:image" content="https://imgur.com/qtzOumT.png" />
         <meta property="og:description" content="在這裡可以找到張哲瑋的作品集、個人經歷和部落格。" />
       </Head>
       <Nav/>
