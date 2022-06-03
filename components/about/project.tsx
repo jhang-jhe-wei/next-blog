10afb1768b90e9c56ad2e9fb2062a643c89a6bd8
wells <jhang0912407249@gmail.com>
2022-06-03 21:28:24 +0800

    Add flex-1 in project

--- components/about/project.tsx
+++ components/about/project.tsx
@@ -11,7 +11,7 @@ export default function Project({project, index}: ProjectComponentProps) {
   return (
     <li ref={element} className={`hover:scale-110 bg-white dark:bg-primary-opacity mx-auto max-w-xs xl:max-w-full flex xl:flex-row flex-col mb-10 transition-opacity-and-transform duration-500 ${reveal? 'opacity-100' : 'translate-y-20 opacity-0'} ${index % 2 == 0? "": "xl:flex-row-reverse"} print:opacity-100 print:translate-y-0`}>
       <img className="object-scale-down object-center bg-gray-300 h-60 w-80" src={project.image} alt=""/>
-      <div className="flex flex-col justify-between px-7 py-7">
+      <div className="flex flex-col justify-between flex-1 px-7 py-7">
         <div>
           <h3 className="text-xl font-bold text-secondary">{project.title}</h3>
           <p className="mt-5 text-primary dark:text-white line-clamp-5 xl:line-clamp-3">{project.description}</p>
